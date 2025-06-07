a820553ceaac84c0c4f40dec5dea3303e8ba491e
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2025-06-07 15:42:57 +0200

    Add PackageReadme (#8)

--- examples/ConsoleAppExample/Models/Document.cs
+++ examples/ConsoleAppExample/Models/Document.cs
@@ -3,6 +3,8 @@ namespace ConsoleAppExample.Models;
 public class Document
 {
     public int Id { get; set; }
+
     public string? Title { get; set; }
-    public string? Description { get; set; }
+
+    public string? Authorization { get; set; }
 }
